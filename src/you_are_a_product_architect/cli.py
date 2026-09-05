100f6e63abb3f0361025059e3e9090d76b79fed7
LUOXIAO92 <luoxiao9231@gmail.com>
2026-09-05 23:10:46 +0900

    Integrate Team-accepted Tickets through validated dev merges

--- src/you_are_a_product_architect/cli.py
+++ src/you_are_a_product_architect/cli.py
@@ -7,6 +7,7 @@ from .delivery_state import delivery_state
 from .project_setup import setup
 from .skill_check import doctor
 from .ticket_graph import ticket
+from .ticket_integration import integrate_command
 
 
 @click.group()
@@ -18,4 +19,5 @@ main.add_command(setup)
 main.add_command(doctor)
 main.add_command(worldline)
 main.add_command(ticket)
+ticket.add_command(integrate_command)
 main.add_command(delivery_state)
